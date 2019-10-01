c93cad49ef7b4eb8916cc4d0f296c11d08fe09d6
zhuhuixiu <56020259+zhuhuixiu@users.noreply.github.com>
2019-10-01 17:03:17 +0800

    Add files via upload

---
+++ TestConstructors.java
@@ -0,0 +1,27 @@
+package constructors;
+
+public class TestConstructors {
+
+	public static void main(String[] args) {
+		
+		//Dog qiangni  = new Dog();  //做对象的初始化操作
+		//Dog qiangxiao = new Dog();
+		//qiangni.print();
+		//qiangxiao.print();
+		
+		
+		//使用有参数的构造方法进行对象的创建
+//		String name = "小白";
+//		int health = 78;
+//		int love = 23;
+//		String strain = "田园犬";
+		Dog xiaobai = new Dog("小白",78,23,"田园犬");
+		xiaobai.print();
+//		Dog xiaohuang = new Dog("小黄",88,23,"泰迪");
+//		xiaohuang.print();
+		
+		
+
+	}
+
+}
